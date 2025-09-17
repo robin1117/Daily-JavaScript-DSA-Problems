31e028707ebf8fe6b9e0f9c250e5950b447c61fd
Devin <Robinsingh1117@gmail.com>
2025-09-17 21:22:35 +0530

    Happy Coding Day 3

---
+++ FibonacciSequence.js
@@ -0,0 +1,33 @@
+/*
+-------------------------------------------------
+📝 Problem: Fibonacci Sequence
+-------------------------------------------------
+The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones. When starting with 0 and 1, the first 10 numbers in the sequence are 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
+
+Given an array containing the first two numbers of a Fibonacci sequence, and an integer representing the length of the sequence, return an array containing the sequence of the given length.
+
+Your function should handle sequences of any length greater than or equal to zero.
+If the length is zero, return an empty array.
+Note that the starting numbers are part of the sequence.
+-------------------------------------------------
+✅ Source: FreeCodeCamp DSA
+-------------------------------------------------
+*/
+
+function fibonacciSequence(startSequence, length) {
+  if (length == 1) {
+    return [startSequence[0]]
+  }
+  if (length == 2) {
+    return startSequence
+  }
+
+  let arr = [...startSequence]
+  for (let i = 0; i < (length - 2); i++) {
+    let x = arr[i]+arr[i+1]
+    arr.push(x)
+  }
+  return arr
+}
+
+console.log(fibonacciSequence([0, 1], 20))
--- README.md
+++ README.md
@@ -4,9 +4,10 @@ I am solving **one coding challenge every day** and uploading my solutions here.
 
 ## 📅 Progress Tracker
 
-| Date       | Challenge            | Solution Link                   |
-| ---------- | -------------------- | ------------------------------- |
-| 2025-09-13 | Find Missing Numbers | [Solution](./missingNumbers.js) |
-| 2025-09-13 | Vowel Balance        | [Solution](./vowelBalance.js)   |
-| 2025-09-14 | Word Frequency       | [Solution](./wordFrequency.js)  |
-| 2025-09-15 | Base Check           | [Solution](./baseCheck.js)      |
+| Date       | Challenge            | Solution Link                      |
+| ---------- | -------------------- | ---------------------------------- |
+| 2025-09-13 | Find Missing Numbers | [Solution](./missingNumbers.js)    |
+| 2025-09-13 | Vowel Balance        | [Solution](./vowelBalance.js)      |
+| 2025-09-14 | Word Frequency       | [Solution](./wordFrequency.js)     |
+| 2025-09-15 | Base Check           | [Solution](./baseCheck.js)         |
+| 2025-09-17 | Fibonacci Sequence   | [Solution](./FibonacciSequence.js) |
